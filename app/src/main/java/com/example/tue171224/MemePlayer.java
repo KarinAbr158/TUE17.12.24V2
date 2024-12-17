b1e472a285425af29a77c4e75dface2491e6abce
Karin <abramowitzkarin@gmail.com>
2024-12-17 13:46:40 +0200

    Initial commit

---
+++ app/src/main/java/com/example/tue171224/MemePlayer.java
@@ -0,0 +1,13 @@
+package com.example.tue171224;
+
+public class MemePlayer {
+    String play;
+
+    public MemePlayer(String play) {
+        this.play = play;
+    }
+
+    public String getPlay() {
+        return play;
+    }
+}
